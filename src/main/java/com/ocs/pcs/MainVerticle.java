a2fe7e52bdab057de08f6499caead315f97b405f
bpiepers <bas.piepers@asml.com>
2018-08-21 07:37:56 +0200

    Starting point for preparation of project

---
+++ src/main/java/com/ocs/pcs/MainVerticle.java
@@ -0,0 +1,12 @@
+package com.ocs.pcs;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        System.out.println("Hi!");
+    }
+
+}
